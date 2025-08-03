0e388406279785905b79dd374bbb123fae17b05b
itsme3bdo <139405402+itsme3bdo@users.noreply.github.com>
2025-08-04 01:23:31 +0300

    Create use_a_debugger.py

---
+++ Day-13-Debugging_Practices/use_a_debugger.py
@@ -0,0 +1,30 @@
+import random
+import maths
+
+
+def mutate(a_list):
+    b_list = []
+    new_item = 0
+    for item in a_list:
+        new_item = item * 2
+        new_item += random.randint(1, 3)
+        new_item = maths.add(new_item, item)
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+is_leap(2000)
